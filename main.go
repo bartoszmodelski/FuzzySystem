e137bd6bf8e06f066e3faaa815fde1e0c46f7657
bartoszmodelski <bartosz.modelski.15@aberdeen.ac.uk>
2017-11-02 14:29:47 +0000

    Pass test

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Printf("hello, world\n")
-	var input string
-	fmt.ScanIn(&input)
+    fmt.Printf("hello, world\n");
+    fmt.Printf("hello, world\n");
+    fmt.Printf("hello, world\n");
+    fmt.Printf("hello, world\n");
 }
